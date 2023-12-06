e3d9582f7db1de580933015dc20e1ee4fdc0f305
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-12-06 18:25:09 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,22 +5,20 @@ const codes = document.querySelectorAll('.code');
 // Focus on the first imput
 codes[0].focus();
 
-// If user input value more than 9, input field clear itself
-codes[0].addEventListener('input', () => {
-    if(codes[0].value > 9){
-        codes[0].value = '';
-    }
-    console.log(codes[0].value);
-})
+    codes.forEach((code, index) => {
+        code.addEventListener('keydown', (event) => {
+            if(event.key >= 0 && event.key <= 9 ){
+                setTimeout(() => {
+                    codes[index + 1].focus();
+                }, 10);
+                // Clear input field
+            } else if (event.key === 'Backspace'){
+                codes[index - 1].focus();
+            }
 
-// setTimeout(() => {
-//     codes.forEach((code, index) => {
-//         code.addEventListener('keydown', (event) => {
-//             if(event.key >= 0 && event.key <= 9 ){
-//                 codes[index + 1].focus();
-//             }
-//         });
-//     })
-// }, 1000);
+        });
+    })
+
+
 
 
